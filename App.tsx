55bebdaf5931097434817dc2df103e89fb8dc9c0
zhangcx01 <252946169@qq.com>
2022-10-22 21:42:07 +0800

    feat: 添加redux的配置

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 // In App.js in a new project
 
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import MainNavigator from './src/navigator';
+import {store, onLoadRedux, Provider} from './src/redux';
 
 function App() {
-  return <MainNavigator />;
+  const [loaded, setLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    onLoadRedux(() => setLoaded(true));
+  }, []);
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <Provider store={store}>
+      <MainNavigator />
+    </Provider>
+  );
 }
 
 export default App;
