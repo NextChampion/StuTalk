5b6a09eb49010da53b4853e21624d22dbcd3123d
zhangcx01 <252946169@qq.com>
2022-12-03 16:15:38 +0800

    feat: 添加个人资料页面

--- src/navigator/tab/index.tsx
+++ src/navigator/tab/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import HomeScreen from '../../screens/Home';
 import SettingsScreen from '../../screens/Settings';
+import ProfileScreen from '../../screens/Profile';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,6 +11,7 @@ function TabNavigator() {
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 }
