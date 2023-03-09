f5bd26f824a1ce1e1f9594cc6c4826a3d39ad5dd
zhangcx01 <252946169@qq.com>
2023-03-09 22:20:40 +0800

    feat: 完成注册页面的布局

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,6 +1,14 @@
-import {Text, Button} from 'react-native';
 import React from 'react';
 import {dispatch} from '../../redux';
+import {
+  Box,
+  FormControl,
+  Heading,
+  Input,
+  VStack,
+  Button,
+  Center,
+} from 'native-base';
 import PageContainer from '../../components/PageContainer';
 
 function RegisterScreen() {
@@ -10,8 +18,46 @@ function RegisterScreen() {
 
   return (
     <PageContainer>
-      <Text>LoginScreen</Text>
-      <Button title="Go to Main" onPress={handleLogin} />
+      <Center w={'100%'}>
+        <Box safeArea p="2" w="90%" maxW="290" py="8">
+          <Heading
+            size="lg"
+            color="coolGray.800"
+            _dark={{
+              color: 'warmGray.50',
+            }}
+            fontWeight="semibold">
+            Welcome
+          </Heading>
+          <Heading
+            mt="1"
+            color="coolGray.600"
+            _dark={{
+              color: 'warmGray.200',
+            }}
+            fontWeight="medium"
+            size="xs">
+            Sign up to continue!
+          </Heading>
+          <VStack space={3} mt="5">
+            <FormControl>
+              <FormControl.Label>Email</FormControl.Label>
+              <Input />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Password</FormControl.Label>
+              <Input type="password" />
+            </FormControl>
+            <FormControl>
+              <FormControl.Label>Confirm Password</FormControl.Label>
+              <Input type="password" />
+            </FormControl>
+            <Button mt="2" colorScheme="indigo" onPress={handleLogin}>
+              Sign up
+            </Button>
+          </VStack>
+        </Box>
+      </Center>
     </PageContainer>
   );
 }
