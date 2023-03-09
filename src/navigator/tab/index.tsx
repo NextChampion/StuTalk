96ee26b3354d2707672d1f79e9059059bab6ec66
zhangcx01 <252946169@qq.com>
2023-03-09 22:48:49 +0800

    feat: 添加tab图标

--- src/navigator/tab/index.tsx
+++ src/navigator/tab/index.tsx
@@ -1,17 +1,53 @@
 import * as React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 import HomeScreen from '../../screens/Home';
 import SettingsScreen from '../../screens/Settings';
 import ProfileScreen from '../../screens/Profile';
+import {Text} from 'native-base';
 
 const Tab = createBottomTabNavigator();
 
 function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({color}) => {
+            return <Icon color={color} size={30} name={'home'} />;
+          },
+          tabBarLabel: ({color}) => {
+            return <Text color={color}>首页</Text>;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({color}) => {
+            return <Icon color={color} size={30} name={'cog'} />;
+          },
+          tabBarLabel: ({color}) => {
+            return <Text color={color}>广场</Text>;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({color}) => {
+            return <Icon color={color} size={30} name={'user'} />;
+          },
+          tabBarLabel: ({color}) => {
+            return <Text color={color}>我的</Text>;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,10 +1,21 @@
-import {View, Text} from 'react-native';
+import {Text} from 'react-native';
 import React from 'react';
+import {Avatar} from 'native-base';
+import PageContainer from '../../components/PageContainer';
 
 export default function ProfileScreen() {
   return (
-    <View>
+    <PageContainer>
+      <Avatar
+        bg="green.500"
+        size={'2xl'}
+        alignSelf="center"
+        source={{
+          uri: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+        }}>
+        AJ
+      </Avatar>
       <Text>ProfileScreen</Text>
-    </View>
+    </PageContainer>
   );
 }
