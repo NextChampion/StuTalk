c3cf6051943aa6805286cd5ec2289e9deef38719
张存夏 <252946169@qq.com>
2023-03-14 17:17:23 +0800

    feat: 调整tab样式

--- src/screens/OldBoy/index.tsx
+++ src/screens/OldBoy/index.tsx
@@ -4,6 +4,8 @@ import ScrollableTabView from '../../components/ScrollableTabView';
 
 import HotListScreen from '../HotList';
 import RecommmendScreen from '../Recommend';
+import ScrollableTabBar from '../../components/ScrollableTabView/ScrollableTabBar';
+import Colors from '../../ui/Colors';
 interface IProps {
   navigation?: any;
   tabLabel: string;
@@ -15,9 +17,16 @@ export default function OldBoyScreen(props: IProps) {
       locked
       tabBarUnderlineStyle={styles.tabBarUnderlineStyle}
       tabBarTextStyle={styles.tabBarTextStyle}
-      tabBarActiveTextColor="red">
-      <RecommmendScreen tabLabel="推荐" />
-      <HotListScreen tabLabel="热榜" />
+      renderTabBar={(props: any) => (
+        <ScrollableTabBar {...props} style={{borderWidth: 0}} />
+      )}
+      tabBarActiveTextColor={Colors.indigo[500]}>
+      <RecommmendScreen tabLabel="关注" />
+      <HotListScreen tabLabel="全部" />
+      <HotListScreen tabLabel="校内" />
+      <HotListScreen tabLabel="专业" />
+      <HotListScreen tabLabel="班级" />
+      <HotListScreen tabLabel="热榜4" />
     </ScrollableTabView>
   );
 }
@@ -25,6 +34,7 @@ const styles = StyleSheet.create({
   tabBarUnderlineStyle: {
     backgroundColor: 'red',
     width: 100,
+    height: 0,
   },
   tabBarTextStyle: {
     fontSize: 16,
