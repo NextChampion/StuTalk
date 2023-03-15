fb3bfac10fd6759a1d3747f10a497e58a2d3c1bd
张存夏 <252946169@qq.com>
2023-03-15 17:16:26 +0800

    feat: 完成热榜页面布局

---
+++ src/screens/HotList/components/HotLabel.tsx
@@ -0,0 +1,38 @@
+import React, {FC, PropsWithChildren} from 'react';
+import {Box, Text} from 'native-base';
+import Colors from '../../../ui/Colors';
+
+interface IProps {
+  type: 'new' | 'hot';
+}
+const HotLabel: FC<PropsWithChildren<IProps>> = props => {
+  const {type = 'new'} = props;
+  if (type === 'hot') {
+    return (
+      <Box
+        backgroundColor={Colors.danger[100]}
+        px={1}
+        borderRadius={4}
+        alignItems={'center'}
+        justifyContent={'center'}>
+        <Text fontSize={'xs'} color={Colors.danger[400]}>
+          爆
+        </Text>
+      </Box>
+    );
+  }
+  return (
+    <Box
+      backgroundColor={Colors.indigo[100]}
+      px={1}
+      borderRadius={4}
+      alignItems={'center'}
+      justifyContent={'center'}>
+      <Text fontSize={'xs'} color={Colors.indigo[400]}>
+        新
+      </Text>
+    </Box>
+  );
+};
+
+export default HotLabel;
